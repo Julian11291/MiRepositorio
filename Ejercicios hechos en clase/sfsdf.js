b7436d9755ddf023bdd9343272a3e2ffaa3d7d7b
unknown <julianbetancourt1129@gmail.com>
2022-09-20 20:55:20 -0500

    Commit con ejercicios en clase

---
+++ Ejercicios hechos en clase/sfsdf.js
@@ -0,0 +1,34 @@
+function suma(a,b){
+    return a + b;
+}
+
+var rta = suma (87+93);
+
+console.log (rta);
+
+function cuadratica(a,b,c){
+    r1=(b*-1+Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a)
+    r2=(b*-1+Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a)
+
+}
+
+//*********************** */
+function sumarVector(a,b){
+    return a+b;
+}
+var c=sumarVector(3,4);
+console.log(c);
+
+//buscar...
+var = ar=[]
+function buscar(a,b){
+    for(let i=0 i < tam; i++){
+        vector[i] =Mathround((Math.random())*100);
+
+    }
+    console.log(vector); vector
+}
+var c =buscar(10);
+console.log(c)
+;
+
